43c13373e357869eeab828a0aa6bb481d783a3b6
GuilhermeC0sta <98281473+GuilhermeC0sta@users.noreply.github.com>
2022-11-27 21:19:17 -0300

    ok

--- CodWithTree/program.py
+++ CodWithTree/program.py
@@ -1,7 +1,87 @@
+#######################################################################
 # Necessaria a instalacao do biopython (pip install biopython)
 from Bio import pairwise2
 from Bio.pairwise2 import format_alignment 
 from Bio.Seq import Seq
+
+
+ROOT = "root"
+
+class Node:
+    def __init__(self, data = None, tudo = None, seq1 = None, seq2 = None):
+        self.data = data
+        self.seq1 = seq1
+        self.seq2 = seq2
+        self.tudo = tudo
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.data)
+
+class BinaryTree:
+    def __init__(self, data=None, tudo=None, node=None):
+        if node:
+            self.root = node
+        elif data:
+            node = Node(data)
+            self.root = node
+        else:
+            self.root = None
+
+class BinarySearchTree(BinaryTree):
+    
+    def insert(self, value):
+        parent = None
+        x = self.root
+        while(x):
+            parent = x
+            if value < x.data:
+                x = x.left
+            else:
+                x = x.right
+        if parent is None:
+            self.root = Node(value)
+        elif value < parent.data:
+            parent.left = Node(value)
+        else:
+            parent.right = Node(value)
+
+    def simetric_transversal(self, node = None):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.simetric_transversal(node.left)
+        print(node)
+        if node.right:
+            self.simetric_transversal(node.right)              
+
+    def add(self, node):
+        if node.value < self.value:
+            if self.left is None:
+                self.left = node
+            else:
+                self.left.add(node)
+        else:
+            if self.right is None:
+                self.right = node
+            else:
+                self.right.add(node)
+
+    def max(self, node = ROOT):
+        if node == ROOT:
+            node = self.root
+        while node.right:
+            node = node.right
+        return node.data
+
+    def min(self, node=ROOT):
+        if node == ROOT:
+            node = self.root
+        while node.left:
+            node = node.left
+        return node.data
+
 
 nome1 = "guilherme"
 nome2 = "costa"
@@ -71,6 +151,8 @@ count2 = 0
 
 n = 3     # numero de sequencias
 k = 0
+
+
 while n > 0:
     if n == 3:# eh reduzido 0.5 do score a cada gap consecutivo, ou seja, as sequencias com gaps separados podem ter maior score, assim cumprindo a condicao de que os gaps sejam separados
         alignments = pairwise2.align.globalms(seqt[k], seqt[k + 1], alpha, beta, delta, -0.5)
@@ -80,32 +162,59 @@ while n > 0:
                 seqt[k + 1] = alignment.seqB
                 score = alignment.score
                 count1 = count1 + 1
+                
+                tudo = alignment
+                tree = BinarySearchTree(alignment) #raiz, arvore q vai conter os alinhamentos das duas primeiras sequencias
+                tree.seq1 = alignment.seqA
+                tree.seq2 = alignment.seqB
+                tree.tudo = alignment
+                
+                for alignment in alignments:
+                    BinarySearchTree.insert(tree, alignment)
+                    tree.seq1 = alignment.seqA
+                    tree.seq2 = alignment.seqB
+                    tree.tudo = alignment
+                break
             else:
-                if alignment.score >= score:
-                    seqt[k] = alignment.seqA
-                    seqt[k + 1] = alignment.seqB
-                    score = alignment.score
+                BinarySearchTree.insert(tree, alignment.score)
+                tree.seq1 = alignment.seqA
+                tree.seq2 = alignment.seqB
+                
         n = n - 2
         k = k + 1
+        alignmax1 = BinarySearchTree.max(tree)
     else:
         alignments = pairwise2.align.globalms(seqt[k], seqt[k + 1], alpha, beta, delta, -0.5)        
-        for alignment in alignments: 
+        for align in alignments: 
             if count2 == 0:
-                seqt[k] = alignment.seqA
-                seqt[k + 1] = alignment.seqB
-                scoreaux = alignment.score
+                seqt[k] = align.seqA
+                seqt[k + 1] = align.seqB
+                scoreaux = align.score
                 count2 = count2 + 1
+
+                tudo = align
+                tree1 = BinarySearchTree(align)
+                tree1.seq1 = align.seqA
+                tree1.seq2 = align.seqB
+                tree1.tudo = align
+
+                for align in alignments:
+                    BinarySearchTree.insert(tree1, align)
+                    tree1.seq1 = align.seqA
+                    tree1.seq2 = align.seqB
+                    tree1.tudo = align
+                break
             else:    
-                if alignment.score >= scoreaux:   
-                    seqt[k] = alignment.seqA
-                    seqt[k + 1] = alignment.seqB
-                    scoreaux = alignment.score    
+                BinarySearchTree.insert(tree1, align.score)
+                tree1.seq1 = align.seqA
+                tree1.seq2 = align.seqB
+                BinarySearchTree.simetric_transversal(tree1)
         n = n - 2
         k = k + 1
-
+        alignmax2 = BinarySearchTree.max(tree1)
+        
 print("-----------------------------------------")
 print(f'Sequencia final: {seqt[0]}')
 print(f'Sequencia final: {seqt[1]}')
 print(f'Sequencia final: {seqt[2]}')
-score_total = score + scoreaux
-print(f'Score total = {score_total}')
+print(f'Score Total: {alignmax1.score + alignmax2.score}')
